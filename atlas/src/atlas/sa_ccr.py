270ec0ca3926cddb2063373850b79af77f45dccd
krzysiekbienias <krzysztofbienias@Mac.lan>
2025-01-24 01:15:57 +0100

    sa-ccr: Adding interface for SA-CCR.

--- atlas/src/atlas/sa_ccr.py
+++ atlas/src/atlas/sa_ccr.py
@@ -3,14 +3,54 @@ class StandardApproachCCRInterface:
         pass
 
 
-    def fetch_data(self):
+    def fetch_trades(self):
+        """
+        Fetch trades from the database using Django ORM.
+        Trades are fetched based on the counterparty ID or netting set ID.
+        Only one of these filters should be used at a time.
+
+        :return:
+        """
         pass
 
-    def calulate_replacement_cost(self,id):
+    def calulateSA_replacement_cost(self,netting_set_id):
+        """
+        Calculate the Replacement Cost (RC).
+
+        RC = max(0, ∑ mark_to_market - ∑ collateral_posted)
+
+        :return: Replacement Cost as a Decimal.
+        """
         pass
 
-    def get_potential_future_exposure(self,id):
+    def get_potential_future_exposure(self,netting_set_id):
+        """
+        Calculate Potential Future Exposure (PFE).
+
+        PFE = ∑ (Notional × Supervisory Factor)
+
+        :return: Potential Future Exposure as a Decimal.
+        """
         pass
+
+    def get_supervisory_factor(self,asset_class,maturity):
+        """
+        Retrieve the supervisory factor based on the asset class and maturity of the trade.
+
+        :param asset_class:
+        :param maturity:
+        :return:
+        """
+        pass
+
+    def calculate_ead(self):
+        """
+        Calculate the EAD of the trade.
+        EAD = ALPHA × (RC + PFE)
+        :return:Exposure at dafault as a Decimal.
+        pass
+        """
+
 
 
 
