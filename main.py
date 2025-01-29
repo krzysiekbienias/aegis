ba26e47700ad481c672a5dedede7fd7a0610e566
krzysiekbienias <krzysztofbienias@Mac.lan>
2025-01-29 12:17:50 +0100

    main: Main update and system file updates

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello World")
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
+django.setup()
+from atlas.src.atlas.data_service import YahooDataExtractor,import_data_from_csv
+
+
+if __name__ == '__main__':
+    import_data_from_csv(r'/Users/krzysztofbienias/Documents/Trade_Data_fixed_rules.csv')
+    yahoo_extractor=YahooDataExtractor(tickers="PLNEUR=X",start_period='2025-01-20',end_period='2025-01-20')
+    resu=yahoo_extractor.extract_data()
+    print("THE END")
